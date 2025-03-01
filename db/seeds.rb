45af4043a0d3f241a40bdd32026279df0886e4ff
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-03-01 00:00:59 -0600

    Add create-products-from-CSV functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,45 @@
 #   end
 
 require "faker"
+require "csv"
 
-676.times do
-  product_title = Faker::Food.dish
-  product_price = Faker::Commerce.price
-  product_stock_quantity = Faker::Number.within(range: 0..100)
+Product.destroy_all
+Category.destroy_all
 
-  new_product = Product.new(title: product_title, price: product_price, stock_quantity: product_stock_quantity)
+# Generate products with Faker
+# 676.times do
+#   product_title = Faker::Food.dish
+#   product_price = Faker::Commerce.price
+#   product_stock_quantity = Faker::Number.within(range: 0..100)
 
-  if new_product.save
-    puts "Product created successfully!"
-  else
-    puts "Error creating product: #{product.errors.full_messages}"
-  end
-end
+#   new_product = Product.new(title: product_title, price: product_price, stock_quantity: product_stock_quantity)
+
+#   if new_product.save
+#     puts "Product created successfully!"
+#   else
+#     puts "Error creating product: #{product.errors.full_messages}"
+#   end
+# end
+
+
+# Populate products with CSV
+csv_file = Rails.root.join('db/products.csv')
+   csv_data = File.read(csv_file)
+
+   products = CSV.parse(csv_data, headers: true)
+
+   # If CSV was created by Excel in Windows you may also need to set an encoding type:
+   # products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+   products.first(10).each do |product|
+    category_name = product['category']
+    category = Category.find_or_create_by(name: category_name)
+
+    Product.create(
+      title: product['name'],
+      price: product['price'].to_f,
+      description: product['description'],
+      stock_quantity: product['stock quantity'].to_i,
+      category: category
+    )
+   end
